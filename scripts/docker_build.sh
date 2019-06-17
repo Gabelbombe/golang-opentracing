7d692f814867b66d3b1d84c739e7af4e93136f92
ehime <dodomeki@gmail.com>
2019-06-17 12:28:09 -0700

    bleh

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-docker run --rm -ti -v $GOPATH:/go -w=/go/src/github.com/opentracing/opentracing-example \
+docker run --rm -ti -v $GOPATH:/go -w=/go/src/github.com/Gabelbombe/golang-opentracing \
        golang:1.6-alpine go build -v
